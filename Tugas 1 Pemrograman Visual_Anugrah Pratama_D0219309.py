1697c61f7743178c398f3d36ba49d654fe581d95
AnugrahPratamaa <101029325+AnugrahPratamaa@users.noreply.github.com>
2022-03-15 20:17:26 +0800

    Add files via upload

---
+++ Tugas 1 Pemrograman Visual_Anugrah Pratama_D0219309.py
@@ -0,0 +1,27 @@
+#Nama  : Anugrah Pratama
+#Nim   : D0219309
+#Kelas : Informatika B
+
+#Penjumlahan
+Anggur = 7
+Mangga = 9
+Buah = Anggur + Mangga 
+print(Buah)
+
+#Pengurangan
+Hutang = 20000
+Bayar = 10000
+SisaHutang = Hutang - Bayar
+print("Sisa Hutang Anda Adalah ",SisaHutang)
+
+#Perkalian
+Panjang = 20
+Lebar = 4
+Luas = Panjang * Lebar
+print(Luas)
+
+#Pembagian
+Baju = 20
+Orang = 5
+BajuPerOrang = Baju / Orang
+print("Setiap Orang Mendapatkan Baju Sebanyak ",BajuPerOrang)
