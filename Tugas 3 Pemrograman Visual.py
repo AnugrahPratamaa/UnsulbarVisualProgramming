be0fd41af16ddba75881f17a73236e6167232800
AnugrahPratamaa <101029325+AnugrahPratamaa@users.noreply.github.com>
2022-03-17 20:43:52 +0800

    Add files via upload

---
+++ Tugas 3 Pemrograman Visual.py
@@ -0,0 +1,11 @@
+class Karyawan:
+    def __init__(self, inputNama, inputUmur, inputAlamat):
+        self.nama = inputName
+        self.umur = inputUmur
+        self.alamat = inputAlamat
+
+Karyawan1 = ("Kaco","20","Lutang")
+Karyawan2 = ("Budi","22","Tulu")
+
+print(Karyawan1)
+print(Karyawan2)
