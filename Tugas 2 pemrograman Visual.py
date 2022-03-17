3d9663f967cbebc868c96c02e5364d6d14701c4d
AnugrahPratamaa <101029325+AnugrahPratamaa@users.noreply.github.com>
2022-03-17 15:05:33 +0800

    Add files via upload

---
+++ Tugas 2 pemrograman Visual.py
@@ -0,0 +1,15 @@
+class Unisoviet:
+    pass
+
+unisoviet1 = Unisoviet()
+unisoviet2 = Unisoviet();
+
+unisoviet1.negara = 'Rusia'
+unisoviet1.kekuatan_tempur = 'Sangat Kuat'
+unisoviet1.jumlah_tentara = '20000'
+
+unisoviet2.negara = 'Ukraina'
+unisoviet2.kekuatan_tempur = 'Lumayan'
+unisoviet2.jumlah_tentara = '5000'
+
+print(unisoviet1.__dict__)
