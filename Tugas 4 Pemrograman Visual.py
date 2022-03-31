2ccc949b66ce4d8882a6dbbdac6d211c026f5a37
AnugrahPratamaa <101029325+AnugrahPratamaa@users.noreply.github.com>
2022-03-31 14:38:34 +0800

    Add files via upload

---
+++ Tugas 4 Pemrograman Visual.py
@@ -0,0 +1,8 @@
+class Variable:
+   def __init__(self, Nama, Nim, Panggilan):
+      self.nama      = Nama           # public
+      self.nim       = Alias          # private
+      self.panggilan = Panggilan
+
+Variable = ("Anugrah Pratama", "D0219309", "Anugrah")
+print(Variable)
